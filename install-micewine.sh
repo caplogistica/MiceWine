584e528c9877e76928ce2986777811d16c212c43
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2023-09-01 18:00:35 -0300

    Alpha V3

--- install-micewine.sh
+++ install-micewine.sh
@@ -32,6 +32,10 @@ read xfce
 case $xfce in "S"|"s")
 	pkg ins xfce4 firefox -y
  	break
+  	;;
+   	"N"|"n")
+    	break
+esac
 done
 
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
