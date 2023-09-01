8941a12ba52b0a6f4f4556b5c5869f0f17ff7dbc
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2023-09-01 17:59:00 -0300

    Alpha V3

--- install-micewine.sh
+++ install-micewine.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/sh
 while :; do
 clear
-echo -e "[ MiceWine -- Alpha 2 ]\n"
-echo -e "Você realmente deseja instalar o MiceWine Alpha 2? (S/N)\n:"
+echo -e "[ MiceWine -- Alpha V3 ]\n"
+echo -e "Você realmente deseja instalar o MiceWine Alpha V3? (S/N)\n:"
 read install
 
 case $install in "S"|"s")
@@ -17,17 +17,31 @@ done
 
 termux-setup-storage
 
-echo "Baixando o MiceWine Alpha 2..."
-curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.2/MiceWine-Alpha-2.zip
+echo "Baixando o MiceWine Alpha V3..."
+curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.3/MiceWine-Alpha-V3.zip
 
 echo "Extraindo..."
-unzip -o MiceWine-Alpha-2.zip -d /data/data/com.termux/files/
+unzip -o MiceWine-Alpha-V3.zip -d /data/data/com.termux/files/
 
-rm -rf MiceWine-Alpha-2.zip 
+rm -rf MiceWine-Alpha-V3.zip 
 
-echo -e "MiceWine Alpha 2 - Instalado!\n"
-echo "Pressione qualquer tecla para sair."
+while :; do
+echo -e "Deseja instalar a interface XFCE? (S/N)\n"
+read xfce
 
-read
+case $xfce in "S"|"s")
+	pkg ins xfce4 firefox -y
+ 	break
+done
 
-pkill termux
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+termux-x11 :1 &
+
+virgl_test_server_android &
+
+export DISPLAY=:1
+
+echo -e "MiceWine Alpha V3 - Instalado!\n"
+echo "Digite: "micewine" para começar usar!"
+
+exit
