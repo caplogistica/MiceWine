c1bd25dbb31f763e44eff7fd6c59b83e338fee47
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2023-09-28 20:43:40 -0300

    Update install-micewine.sh

--- install-micewine.sh
+++ install-micewine.sh
@@ -1,17 +1,26 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/data/data/com.termux/files/usr/bin/bash
 while :; do
 clear
-echo -e "[ MiceWine -- Alpha ]\n"
+echo -e "[ MiceWine -- Installer ]\n"
 
-echo -e "Escolha uma versão:\n 1) Alpha V2\n 2) Alpha V3\n 3) Alpha V4 -- PatchFix 1\n 4) Alpha V5"
+echo -e "Escolha uma versão:\n 2) Alpha V2\n 3) Alpha V3\n 4) Alpha V4 -- PatchFix 1\n 5) Alpha V5"
 
 read ver
 
-case $ver in "3")
+case $ver in "2"|"3"|"4"|"5")
+	break
+	;;
+	*)
+esac
+
+done
+
+while :; do
+case $ver in "4")
 	SUFFIX="PatchFix 1"
 esac
 
-echo -e "Você realmente deseja instalar o MiceWine Alpha V$(($ver+1)) $SUFFIX? (S/N)\n:"
+echo -e "Você realmente deseja instalar o MiceWine Alpha V$ver $SUFFIX? (S/N)\n:"
 read install
 
 case $install in "S"|"s")
@@ -28,35 +37,31 @@ termux-setup-storage
 
 echo "Baixando o MiceWine Alpha V$(($ver+1)) $SUFFIX..."
 
-case $ver in "1")
+case $ver in "2")
 	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.2/MiceWine-Alpha-2.zip
  	;;
-  	"2")
+  	"3")
 	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.3/MiceWine-Alpha-V3.zip
  	;;
-  	"3")
+  	"4")
    	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.4-patchfix1/MiceWine-Alpha-V4-Fix-1.zip
-    	;;
-     	"4")
-      	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5/MiceWine-Alpha-V5.zip
-       	;;
-	*)
- 	echo MiceWine Alpha V$(($ver+1)) $SUFFIX não está disponivel.
-  	exit
+    ;;
+    "5")
+    curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5/MiceWine-Alpha-V5.zip
 esac
 
 echo "Extraindo..."
-case $ver in "1")
+case $ver in "2")
 	unzip -o MiceWine-Alpha-2.zip -d /data/data/com.termux/files/
  	;;
-  	"2")
+  	"3")
 	unzip -o MiceWine-Alpha-V3.zip -d /data/data/com.termux/files/
  	;;
-  	"3")
+  	"4")
    	unzip -o MiceWine-Alpha-V4-Fix-1.zip -d /data/data/com.termux/files/
-    	;;
-     	"4")
-      	unzip -o MiceWine-Alpha-V5.zip -d /data/data/com.termux/files/
+    ;;
+ 	"5")
+  	unzip -o MiceWine-Alpha-V5.zip -d /data/data/com.termux/files/
 esac
 
 rm -rf MiceWine-Alpha-V3.zip MiceWine-Alpha-2.zip MiceWine-Alpha-V4-Fix-1.zip MiceWine-Alpha-V5.zip
@@ -74,9 +79,7 @@ case $xfce in "S"|"s")
 esac
 done
 
-echo -e "MiceWine Alpha V$(($ver+1)) $SUFFIX - Instalado!\n"
-echo "Digite: "micewine" para começar usar!"
+echo -e "MiceWine Alpha V$ver $SUFFIX - Instalado!\n"
+echo "Digite "exit", abra o Termux e em seguida\nDigite: "micewine" para começar usar!"
 
-sleep 2
-
-logout
+exit
