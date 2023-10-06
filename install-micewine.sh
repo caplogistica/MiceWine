5c9670ffb6626f3cffbf2be265dbbccea036030a
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2023-10-05 22:56:58 -0300

    Added Alpha V5.2

--- install-micewine.sh
+++ install-micewine.sh
@@ -3,11 +3,11 @@ while :; do
 clear
 echo -e "[ MiceWine -- Installer ]\n"
 
-echo -e "Escolha uma versão:\n 2) Alpha V2\n 3) Alpha V3\n 4) Alpha V4 -- PatchFix 1\n 5) Alpha V5\n 6) Alpha V5.1"
+echo -e "Escolha uma versão:\n 2) Alpha V2\n 3) Alpha V3\n 4) Alpha V4 -- PatchFix 1\n 5) Alpha V5\n 6) Alpha V5.1\n 7) Alpha V5.2"
 
 read ver
 
-case $ver in "2"|"3"|"4"|"5"|"6")
+case $ver in "2"|"3"|"4"|"5"|"6"|"7")
 	break
 	;;
 	*)
@@ -21,6 +21,9 @@ case $ver in "4")
  	;;
 	"6")
  	ver="5.1"
+  	;;
+   	"7")
+    	ver="5.2"
 esac
 
 echo -e "Você realmente deseja instalar o MiceWine Alpha V$ver $SUFFIX? (S/N)\n:"
@@ -54,6 +57,9 @@ case $ver in "2")
     	;;
     	"5.1")
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5.1/MiceWine-Alpha-V5.1.zip
+     	;;
+     	"5.2")
+      	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5.2/MiceWine-Alpha-V5.2.zip
 esac
 
 echo "Extraindo..."
@@ -71,9 +77,12 @@ case $ver in "2")
    	;;
     	"5.1")
     	unzip -o MiceWine-Alpha-V5.1.zip -d /data/data/com.termux/files/
+     	;;
+    	"5.2")
+    	unzip -o MiceWine-Alpha-V5.2.zip -d /data/data/com.termux/files/
 esac
 
-rm -rf install-micewine.sh MiceWine-Alpha-V3.zip MiceWine-Alpha-2.zip MiceWine-Alpha-V4-Fix-1.zip MiceWine-Alpha-V5.zip MiceWine-Alpha-V5.1.zip
+rm -rf MiceWine-Alpha-2.zip MiceWine-Alpha-V3.zip MiceWine-Alpha-V4-Fix-1.zip MiceWine-Alpha-V5.zip MiceWine-Alpha-V5.1.zip MiceWine-Alpha-V5.2.zip 
 
 while :; do
 echo -e "Deseja instalar a interface XFCE? (S/N) (Opcional)\n"
@@ -89,6 +98,6 @@ esac
 done
 
 echo -e "MiceWine Alpha V$ver $SUFFIX - Instalado!\n"
-echo "Digite "exit", abra o Termux e em seguida\nDigite: "micewine" para começar usar!"
+echo -e "Digite "exit", abra o Termux e em seguida\nDigite: "micewine" para começar usar!"
 
 exit
