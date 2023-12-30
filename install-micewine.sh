6f106584161e7d40f01d9792d9f0af95cd88aa41
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2023-12-29 21:04:19 -0300

    Added MiceWine Alpha V6.1 and fix dialog on end (again)

--- install-micewine.sh
+++ install-micewine.sh
@@ -37,49 +37,53 @@ esac
 echo -e "$CHOICE_VERSION" > $TMPDIR/.dialoginfo
 
 case $(uname -m) in "aarch64")
-	./dialog64 Alpha///V6.0 Alpha///V5.3 Alpha///V5.2 Alpha///V5.1 Alpha///V5 Alpha///V4///PatchFix///1 Alpha///V4 Alpha///V3 Alpha///V2
+	./dialog64 Alpha///V6.1 Alpha///V6.0 Alpha///V5.3 Alpha///V5.2 Alpha///V5.1 Alpha///V5 Alpha///V4///PatchFix///1 Alpha///V4 Alpha///V3 Alpha///V2
 	;;
 	*)
-	./dialog Alpha///V6.0 Alpha///V5.3 Alpha///V5.2 Alpha///V5.1 Alpha///V5 Alpha///V4///PatchFix///1 Alpha///V4 Alpha///V3 Alpha///V2
+	./dialog Alpha///V6.1 Alpha///V6.0 Alpha///V5.3 Alpha///V5.2 Alpha///V5.1 Alpha///V5 Alpha///V4///PatchFix///1 Alpha///V4 Alpha///V3 Alpha///V2
 esac
 
 termux-setup-storage
 
-case $(cat $TMPDIR/.dialogout) in "9")
+case $(cat $TMPDIR/.dialogout) in "10")
     	echo "$DOWNLOADING MiceWine Alpha V2..."	
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.2/MiceWine-Alpha-2.zip
     	;;
-   	"8")
+   	"9")
     	echo "$DOWNLOADING MiceWine Alpha V3..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.3/MiceWine-Alpha-V3.zip
     	;;
-    	"7")
+    	"8")
     	echo "$DOWNLOADING MiceWine Alpha V4..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.4/MiceWine-Alpha-V4.zip
     	;;
-   	 "6")
+   	"7")
     	echo "$DOWNLOADING MiceWine Alpha V4 - PatchFix 1..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.4-patchfix1/MiceWine-Alpha-V4-Fix-1.zip
     	;;
-    	"5")
+    	"6")
     	echo "$DOWNLOADING MiceWine Alpha V5..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5/MiceWine-Alpha-V5.zip
     	;;
-   	 "4")
+   	"5")
     	echo "$DOWNLOADING MiceWine Alpha V5.1..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5.1/MiceWine-Alpha-V5.1.zip
     	;;
-    	"3")
+    	"4")
     	echo "$DOWNLOADING MiceWine Alpha V5.2..."
     	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5.2/MiceWine-Alpha-V5.2.zip
     	;;
-	"2")
+	"3")
 	echo "$DOWNLOADING MiceWine Alpha V5.3..."
 	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.5.3/MiceWine-Alpha-V5.3.zip
 	;;
-	"1")
+	"2")
 	echo "$DOWNLOADING MiceWine Alpha V6.0..."
 	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.6/MiceWine-Alpha-V6.0-Xmas.zip
+ 	;;
+  	"1")
+  	echo "$DOWNLOADING MiceWine Alpha V6.1..." 
+   	curl -# -L -O https://github.com/KreitinnSoftware/MiceWine/releases/download/v0.0.6.1/MiceWine-Alpha-V6.1.zip
 esac
 
 case $(cat $TMPDIR/.dialogout) in "9")
@@ -108,19 +112,29 @@ case $(cat $TMPDIR/.dialogout) in "9")
  	;;
  	"1")
   	unzip -o MiceWine-Alpha-V6.0-Xmas.zip -d /data/data/com.termux/files/
+   	;;
+    	"1")
+     	unzip -o MiceWine-Alpha-V6.1.zip -d /data/data/com.termux/files/
 esac
 
-rm -rf *.zip dialog*
+rm -rf *.zip
 
 echo -e "$INSTALLXFCE" > $TMPDIR/.dialoginfo
 
-micewine-dialog $NOYES
+case $(uname -m) in "aarch64")
+	./dialog64 $NOYES
+	;;
+	*)
+	./dialog $NOYES
+esac
 
 case $(cat $TMPDIR/.dialogout) in "1")
 	;;
 	"2")
 	pkg ins xfce4 firefox -y
 esac
+
+rm -rf dialog*
 
 echo -e "$INSTALLDONE"
 
