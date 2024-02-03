e377899e6e274c1bfdcc1345836fdf7ae864a64b
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2024-02-02 22:17:10 -0300

    Added 32bit Version Checking

--- update-micewine.sh
+++ update-micewine.sh
@@ -1,7 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 INSTALLED_MICEWINE_VERSION=$(micewine --version)
 
-LATEST_MICEWINE_VERSION="V7.1"
+LATEST_MICEWINE_64_VERSION="V7.1"
+LATEST_MICEWINE_32_VERSION="V6.2"
 
 echo "Installed Version: $INSTALLED_MICEWINE_VERSION"
 
@@ -12,10 +13,20 @@ case $(uname -m) in "aarch64")
   IS64BITS=0
 esac
 
-case $INSTALLED_MICEWINE_VERSION in *"$LATEST_MICEWINE_VERSION"*)
-  echo "Your MiceWine installation is up to date."
-  exit
-  ;;
-  *)
-  echo "Your MiceWine installation is out of date"
-esac
+if [ $IS64BITS == 1 ]; then
+  case $INSTALLED_MICEWINE_VERSION in *"$LATEST_MICEWINE_64_VERSION"*)
+    echo "Your MiceWine installation is up to date."
+    exit
+    ;;
+    *)
+    echo "Your MiceWine installation is out of date"
+  esac
+else 
+  case $INSTALLED_MICEWINE_VERSION in *"$LATEST_MICEWINE_32_VERSION"*)
+    echo "Your MiceWine installation is up to date."
+    exit
+    ;;
+    *)
+    echo "Your MiceWine installation is out of date"
+  esac
+fi
