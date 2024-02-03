57c4c063b3ece904b680906ebbc7f0312360a0e8
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2024-02-02 22:13:08 -0300

    Added a Basic Check of Version

--- update-micewine.sh
+++ update-micewine.sh
@@ -1,4 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 INSTALLED_MICEWINE_VERSION=$(micewine --version)
 
+LATEST_MICEWINE_VERSION="V7.1"
+
 echo "Installed Version: $INSTALLED_MICEWINE_VERSION"
+
+case $(uname -m) in "aarch64")
+  IS64BITS=1
+  ;;
+  *)
+  IS64BITS=0
+esac
+
+case $INSTALLED_MICEWINE_VERSION in *"$LATEST_MICEWINE_VERSION"*)
+  echo "Your MiceWine installation is up to date."
+  exit
+  ;;
+  *)
+  echo "Your MiceWine installation is out of date"
+esac
